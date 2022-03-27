9338cb8d140f49213d69f056635a3507d15eb1eb
typeluis <typeluis@gmail.com>
2022-03-27 03:25:37 -0400

    comment

--- pages/index.js
+++ pages/index.js
@@ -48,18 +48,18 @@ const HomePage = (props) => {
 export default HomePage
 
 
-export const getServerSideProps = async (context) => {
-  const products = await client.product.fetchAll(); // Fetch product
+// export const getServerSideProps = async (context) => {
+//   const products = await client.product.fetchAll(); // Fetch product
 
-  const infos = await client.shop.fetchInfo(); // Fetch shop Info if you think about SEO and title and ... to your page
+//   const infos = await client.shop.fetchInfo(); // Fetch shop Info if you think about SEO and title and ... to your page
 
-  const policies = await client.shop.fetchPolicies(); // fetch shop policy if you have any 
+//   const policies = await client.shop.fetchPolicies(); // fetch shop policy if you have any 
 
-  return {
-    props: {
-      infos: JSON.parse(JSON.stringify(infos)),
-      policies: JSON.parse(JSON.stringify(policies)),
-      products: JSON.parse(JSON.stringify(products)),
-    },
-  };
-};
+//   return {
+//     props: {
+//       infos: JSON.parse(JSON.stringify(infos)),
+//       policies: JSON.parse(JSON.stringify(policies)),
+//       products: JSON.parse(JSON.stringify(products)),
+//     },
+//   };
+// };
