68524c03676af10eb3df6dcd9a9c088ea7edc263
typeluis <typeluis@gmail.com>
2022-08-30 17:34:43 -0400

    category

---
+++ pages/Shop/[categories]/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+import Banner from '../../../component/Banner';
+
+const Category = () => {
+    const router = useRouter();
+    console.log(router)
+    return (
+        <Banner name={router.query.categories} img={'https://images.unsplash.com/photo-1653427895631-f73d88ecb5a6?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1072'} />
+    )
+}
+
+export default Category
