d92eff09f784b86fa94bb0aa60a2eea890fa8e5d
typeluis <typeluis@gmail.com>
2022-08-30 22:40:15 -0400

    adding env

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    SHOPIFY_STORE_FRONT_ACCESS_TOKEN: 'a6d5f971db750866a35ec6a05f466445',
-
-    SHOPIFY_STORE_DOMAIN:
-      'first-test-next.myshopify.com'
-  },
 }
 
 
