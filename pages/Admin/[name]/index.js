1afa975f03a7aa9937f603c0f9a4acdc83fa5d9b
typeluis <typeluis@gmail.com>
2022-08-30 23:26:53 -0400

    adding env

---
+++ pages/Admin/[name]/index.js
@@ -0,0 +1,24 @@
+import { useRouter } from 'next/router'
+import React from 'react'
+
+const Admin = () => {
+    const router = useRouter()
+    const name = router.query.name
+
+    name === process.env.SITE
+
+    return (
+        <>
+
+            {name === process.env.SITE &&
+
+                <div>
+                    hello
+                </div>
+
+            }
+        </>
+    )
+}
+
+export default Admin
