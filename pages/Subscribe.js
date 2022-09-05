f20daa3cdaf1605432dc134e33721149cb8d4c3e
typeluis <typeluis@gmail.com>
2022-09-05 01:02:28 -0400

    adding images

--- pages/Subscribe.js
+++ pages/Subscribe.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import Banner from '../component/Banner'
+import Left from '../component/Overlap_Images/Left'
+import Right from '../component/Overlap_Images/Right'
+import Main_Carousel from '../component/Main_carousel/Main_Carousel'
+
 
 const Subscribe = () => {
     return (
-        <Banner name={'Subscribe'} img={'https://images.unsplash.com/photo-1653427472957-b3b3f313d719?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170'}  desc={'A bag built for life'}/>
+        <>
+
+            <Banner name={'Subscribe'} img={'https://images.unsplash.com/photo-1653427472957-b3b3f313d719?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170'} desc={'A bag built for life'} />
+
+            <Left />
+            <Right />
+            <Main_Carousel />
+            <Left />
+            <Right />
+            <Main_Carousel />
+
+        </>
     )
 }
 
